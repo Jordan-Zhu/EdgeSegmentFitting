d89c2f7da3cb43f2fe85992ff5d7abfea2c83cd3
Jordan Zhu <qwertybot1@gmail.com>
2016-06-02 18:55:40 -0400

    Add files via upload

--- PEL.py
+++ PEL.py
@@ -5,14 +5,18 @@ import Queue
 
 import time, random
 
-UP_LEFT =    1
-UP =         2
-UP_RIGHT =   3
-RIGHT =      4
+UP_LEFT = 1
+UP = 2
+UP_RIGHT = 3
+RIGHT = 4
 DOWN_RIGHT = 5
-DOWN =       6
-DOWN_LEFT =  7
-LEFT =       8
+DOWN = 6
+DOWN_LEFT = 7
+LEFT = 8
+
+global checkDir
+global compDir
+
 
 class MyQueue:
     # QSIZE = 8
@@ -44,7 +48,7 @@ class MyQueue:
         self.items.insert(self._rear, dir)
         if (self._rear >= self.qsize):
             self._rear = 0
-        if(self._noItems < self.qsize):
+        if self._noItems < self.qsize:
             self._noItems += 1
 
     def getCardinalDir(self, analysisDir):
@@ -77,17 +81,17 @@ class MyQueue:
 
     def getDiagonalDir(self, analysisDir):
         global checkDir
-        compare = np.zeros((2,), dtype = np.int)
+        compare = np.zeros((2,), dtype=np.int)
 
         # Sides to check for each diagonal.
         if analysisDir == UP_LEFT:
-            checkDir = {UP, LEFT}
+            checkDir = [UP, LEFT]
         elif analysisDir == UP_RIGHT:
-            checkDir = {UP, RIGHT}
+            checkDir = [UP, RIGHT]
         elif analysisDir == DOWN_RIGHT:
-            checkDir = {DOWN, RIGHT}
+            checkDir = [DOWN, RIGHT]
         else:
-            checkDir = {DOWN, LEFT}
+            checkDir = [DOWN, LEFT]
 
         # Compare to find the strongest side.
         for i in xrange(0, self._noItems):
@@ -104,17 +108,18 @@ class MyQueue:
             return checkDir[1]
 
     def computeNextDir(self, analysisDir):
-        global nextDir
+        global compDir
         # Check the sets to determine the type of direction.
         cardinalDir = frozenset([LEFT, RIGHT, UP, DOWN])
         diagonalDir = frozenset([UP_LEFT, UP_RIGHT, DOWN_RIGHT, DOWN_LEFT])
 
         if analysisDir in cardinalDir:
-            nextDir = self.getCardinalDir(analysisDir)
+            compDir = self.getCardinalDir(analysisDir)
         elif analysisDir in diagonalDir:
-            nextDir = self.getDiagonalDir(analysisDir)
+            compDir = self.getDiagonalDir(analysisDir)
 
-        return nextDir
+        return compDir
+
 
 class CreateEdgeSeg:
     # initialization
@@ -140,13 +145,16 @@ class CreateEdgeSeg:
 
 
         while 1:
+            # Check bounds.
             if row <= 0 or row >= height - 1:
                 return count
             if col <= 0 or col >= width:
                 return count
 
-            rows = edgeImg.shape[0]
-            cols = edgeImg.shape[1]
+            pixels[count].r = row
+            pixels[count].c = col
+            # row = edgeImg.shape[0]
+            # col = edgeImg.shape[1]
             count += 1
 
             # Add the current direction to the queue.
@@ -156,100 +164,146 @@ class CreateEdgeSeg:
                 nextDir = Q.computeNextDir(UP_LEFT)
 
                 # Up-Left?
-                if edgeImg[(row - UR[0]) * width + (col + UR[1])]:
+                if edgeImg[(row + UR[0]) * width + (col + UR[1])]:
                     if nextDir == UP:
                         # Up?
-                        if edgeImg[(row - U[0]) * width + (col + U[1])]:
-                            pixels[count].row = row - 1
-                        #
+                        if edgeImg[(row + U[0]) * width + (col + U[1])]:
+                            pixels[count].r = row + U[0]
+                            pixels[count].c = col + U[1]
+                            count += 1
+                            edgeImg[(row - UR[0]) * width + col] = 0
+                        # Left?
+                        elif edgeImg[(row + L[0]) * width + col + L[1]]:
+                            pixels[count].r = row + L[0]
+                            pixels[count].c = col + L[1]
+                            count += 1
+                            edgeImg[row * width + col + L[1]] = 0
+                    else:
+                        # Left?
+                        if edgeImg[(row + L[0]) * width + col + L[1]]:
+                            pixels[count].r = row + L[0]
+                            pixels[count].c = col + L[1]
+                            count += 1
+                            edgeImg[row * width + col + L[1]] = 0
+
+                        # Up?
+                        elif edgeImg[(row + U[0]) * width + col]:
+                            pixels[count].r = row + U[0]
+                            pixels[count].c = col + U[1]
+                            count += 1
+                            edgeImg[(row + U[0]) * width + col] = 0
+
+                    row += UL[0]
+                    col += UL[1]
+                    dir = UP_LEFT
+                    continue
+
+                if nextDir == UP:
+                    # Up
+                    if edgeImg[(row + U[0]) * width + (col + U[1])]:
+                        row -= 1
+                        dir = UP
+                        continue
+
+                    # Left
+                    if edgeImg[(row + L[0]) * width + (col + L[1])]:
+                        col -= 1
+                        dir = LEFT
+                        continue
+
+                    # Up-Right
+                    if edgeImg[(row + UR[0]) * width + (col + UR[1])]:
+                        row -= 1
+                        col += 1
+                        dir = UP_RIGHT
+                        continue
+
+                    # Down-Left
+                    if edgeImg[(row + DL[0]) * width + (col + DL[1])]:
+                        row += 1
+                        col -= 1
+                        dir = DOWN_LEFT
+                        continue
+
+                    # Right
+                    if edgeImg[(row + R[0]) * width + (col + R[1])]:
+                        col += 1
+                        dir = RIGHT
+                        continue
+
+                    # Down
+                    if edgeImg[(row + D[0]) * width + (col + D[1])]:
+                        row += 1
+                        dir = DOWN
+                        continue
+                else:
+                    # Left
+                    if edgeImg[(row + L[0]) * width + (col + L[1])]:
+                        col -= 1
+                        dir = LEFT
+                        continue
+
+                    # Up
+                    if edgeImg[(row + U[0]) * width + (col + U[1])]:
+                        row -= 1
+                        dir = UP
+                        continue
+
+                    # Down-Left
+                    if edgeImg[(row + DL[0]) * width + (col + DL[1])]:
+                        row += 1
+                        col -= 1
+                        dir = DOWN_LEFT
+                        continue
+
+                    # Up-Right
+                    if edgeImg[(row + UR[0]) * width + (col + UR[1])]:
+                        row -= 1
+                        col += 1
+                        dir = UP_RIGHT
+                        continue
+
+                    # Down
+                    if edgeImg[(row + D[0]) * width + (col + D[1])]:
+                        row += 1
+                        dir = DOWN
+                        continue
+
+                    # Right
+                    if edgeImg[(row + R[0]) * width + (col + R[1])]:
+                        col += 1
+                        dir = RIGHT
+                        continue
+
+                # Nowhere to go
+                return count
 
 
+            elif dir == UP:
+                # Up
+                if edgeImg[(row + U[0]) * width + col]:
+                    row -= 1
+                    dir = UP
+                    continue
 
+                # Should we check LEFT or RIGHT first?
+                nextDir = Q.computeNextDir(LEFT)
 
+                if nextDir == LEFT:
+                    # Up-Left
+                    if edgeImg[(row + UL[0]) * width + (col + UL[1])]:
+                        if edgeImg[row * width + (col + UL[1])]:
+                            edgeImg[row * width + (col + UL[1])] = 0
+                            pixels[count].r = row
+                            pixels[count].c = col + UL[1]
+                            count += 1
 
+                    # Up-Right
+                    if edgeImg[(row + UR[0]) * width + (col + UR[1])]:
+                        if edgeImg[row * width + (col + UR[1])]:
+                            edgeImg[row * width + (col + UR[1])] = 0
+                            pixels[count].r = row
+                            pixels[count].c = col + UR[1]
+                            count += 1
 
-
-# Algorithm:
-#
-# ==== Step 2: EdgeSegment Creation with 8-Directional Walk ====
-# Define 8 movement directions.
-#
-# Next direction prediction engine.
-# Create Queue with size 8 to push in last 8 directions and it
-# makes a prediction for the next direction.
-#
-# Queue has functions Add and ComputeNextDir.
-#
-# Queue starts with noItems and rear set to zero.
-# 'noItems' counts the number of items. We can use this to iterate
-# through the Queue.
-# 'rear' is for adding a direction to the Queue. Keep this pushing
-# new elements to the Queue by resetting it to zero when it gets
-# greater than the QSIZE.
-#
-# ------- Functions --------
-# Add(int dir)
-# Adds direction to the queue
-# if rear becomes larger than QSIZE, reset rear = 0
-# if noItems is less than QSIZE, noItems++
-# --- end-Add ---
-#
-# ComputeNextDir(int analysisDir)
-# Allow computer to pick the direction point.
-# Hold the computed path in an array size 2. This will determine
-# the direction to take.
-# 1. If the direction is LEFT or RIGHT,
-#    - Go through the Queue, 0 to i < noItems
-#    - If the Queue has directions that are neither, continue looping.
-#    - Add to LEFT (0) or RIGHT (1) every time one of those occurs in the Queue.
-#    - Whichever position has a higher score, return that direction.
-# 2. Else if direction is UP or DOWN,
-#    - Go through the Queue, 0 to i < noItems
-#    - If dir is LEFT or RIGHT, continue looping.
-#    - Add c[0] whenever UP, UP_LEFT, or UP_RIGHT occur.
-#    - Else add c[1], hence one of the DOWN directions.
-#    - Whichever position has a higher score, return that direction.
-#      (This should probably be made into it's own function since each move uses it.)
-# We have specific cases for when direction is diagonal.
-# 3. Else if direction is UP_LEFT,
-#    - Go through the Queue, 0 to i < noItems
-#    - If Q has UP, c[0]++, else if Q has LEFT, c[1]++.
-#    - Determine which one to return, c[0] >= c[1], return UP. Else return LEFT.
-# 4. Direction is UP_RIGHT,
-#    - Go through the Queue, 0 to i < noItems
-#    - If Q has UP, c[0]++, else if Q has RIGHT, c[1]++.
-#    - Determine which one to return, c[0] >= c[1], return UP. Else return RIGHT.
-# 5. Direction is DOWN_RIGHT,
-#    - Go through the Queue, 0 to i < noItems
-#    - If Q has DOWN, c[0]++, else if Q has RIGHT, c[1]++.
-#    - Determine which one to return, c[0] >= c[1], return DOWN. Else return RIGHT.
-# 4. Else direction is DOWN_LEFT,
-#    - Go through the Queue, 0 to i < noItems
-#    - If Q has DOWN, c[0]++, else if Q has LEFT, c[1]++.
-#    - Determine which one to return, c[0] >= c[1], return DOWN. Else return LEFT.
-# These diagonals could also be made into a separate function, with args
-# for diagonal, and absolute one and absolute two (eg. UP_RIGHT, UP, RIGHT, noItems, C).
-# --- end-ComputeNextDir ---
-#
-# ------ 8 Directional Walk with Prediction ------
-# This is used by PELWalk8Dirs.
-#
-# Walk8Dirs(edgeImg, width, height, int rows, int cols, int dir, Pixels)
-#
-# Define Queue as Q.
-# count = 0. Counts the number of pixels in forming an edge segment.
-# edgeImg[r* width + c] = 0. Not sure what this does, but it's used later on.
-#
-# Base cases. If rows or columns are out of bounds.
-# if r <= 0 or r >= height - 1; return count
-# if c <= 0 or c >= width - 1; return count
-#
-# pixels[count].r = r
-# pixels[count].c = c
-# count++
-#
-# Add the current direction to Queue.
-# Q.Add(dir)
-#
-#
 
