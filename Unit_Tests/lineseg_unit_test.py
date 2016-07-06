52e3951159520cd29c0a3732a613761602980df2
Jordan Zhu <qwertybot1@gmail.com>
2016-07-06 19:42:54 -0400

    Add files via upload

--- Unit_Tests/lineseg_unit_test.py
+++ Unit_Tests/lineseg_unit_test.py
@@ -1,16 +1,20 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
-from ES_Drawing.lineseg2 import lineseg2
+from ES_Drawing.lineseg import lineseg
+from ES_Drawing.drawedgelist import drawedgelist
 
-def find_contours():
-    im = cv2.imread('circle.png')
+
+
+def find_contours(im):
+    # im = cv2.imread('circle.png')
     imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # print contours
-    cv2.drawContours(im, contours, -1, (0, 255, 0), 3)
+    # show contours
+    # cv2.drawContours(im, contours, -1, (0, 255, 0), 1)
 
     # Display the image.
     # cv2.imshow("window", im)
@@ -19,10 +23,102 @@ def find_contours():
 
     return contours
 
-if __name__ == '__main__':
 
-    edgelist = np.asarray(find_contours())
-    num_edges = edgelist.shape[1]
+if __name__ == '__main__':
+    img = cv2.imread('test.png')
+
+    find_len = find_contours(img)
+    len = len(find_len)
+
+    data = np.asarray(find_contours(img))
+    print 'data shape ', data.shape[0]
+
+    seglist = lineseg(data, tol=2)
+
+    # for i in seglist[0][:, 0]:
+    #     x.append(seglist[0][i, 0])
+    x = seglist[0][:, 0]
+    y = seglist[0][:, 1]
+    print 'x ', x[0]
+    print 'y ', seglist[0][:, 1]
+
+    for n in seglist:
+        for i in range(x.size - 1):
+            cv2.line(img, (x[n], y[n]), (x[n + 1], y[n + 1]), (0, 255, 255), thickness=2)
+    #
+    # cv2.imshow("window", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # drawedgelist(seglist, rowscols=[480, 640])
+
+
+    # for n in range(x.shape[0] - 1):
+    #     cv2.line(img, (x[n], y[n]), (x[n + 1], y[n + 1]), (0, 255, 255), thickness=2)
+    #     plt.plot(x[n], y[n])
+    #     plt.hold('on')
+    # plt.show()
+    # cv2.imshow("window", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # reshape array
+    temp = []
+    for i in xrange(data.shape[0]):
+        arr = np.squeeze(data[i])
+        temp.append(arr)
+
+    # temp = np.asarray(temp)
+    # print 'x ', temp[0][:, 0]
+    # print 'y ', temp[0][:, 1]
+
+    # rebuilt = np.concatenate((data[0], data[1]), axis=0)
+    # print 'new shape ', rebuilt.shape
+    # y = len(test)
+    # test = np.resize(test, (lent, y, 1, 2))
+    # print data
+
+    # Input cell array of edgelists and tolerance.
+    # seglist = lineseg(data, len, tol=2)
+    # print seglist.dtype
+
+    # colon indicates to go through all the elements in this dimension.
+    # x = seglist[0, :, 0]
+    # y = seglist[0, :, 1]
+    # print 'x ', data.shape
+    # print 'y ', y
+    # print y.shape[0]
+
+    # pts = np.asarray([x, y], dtype=np.int32)
+    # print pts.dtype
+
+    # cv2.polylines(img, [x, y], False, (0, 255, 255))
+
+    # list = np.asarray(seglist[0], dtype=np.int32)
+    # print list.shape
+
+    # print seglist[0]
+    # seglist = seglist.reshape((-1, 1, 2))
+    # print 'seglist: ', seglist.shape[0]
+    # print 'seglist shape: ', type(seglist[0])
+
+    # draw = drawedgelist(list, rowscols=[480, 640])
+
+    # edgelist = np.reshape(edgelist, (edgelist.shape[0], -1, 1, 2))
+    # num_edges = edgelist.shape[1]
+    # print 'edgelist shape', edgelist.shape, ' length ', num_edges
+    # edgelist = np.expand_dims(edgelist, axis = 1)
+    # print 'length = ', edgelist.shape[0]
+    # print 'edgelist shape = ', edgelist.shape
+    # edgelist = find_contours()
+
+    # edgelist = np.asarray(find_contours(img))
+    # y = np.concatenate(edgelist, axis=0)
+    # y = np.expand_dims(y, axis=0)
+    # edgelist = np.reshape(edgelist, (-1, 1, 2))
+    # print 'shape = ', y
+    # print y[:, 0, 1]
+    # shape (num arrays, length of array in total, num rows = 1, num cols = 2)
 
     # seglist = []
     # print edgelist
@@ -51,7 +147,3 @@ if __name__ == '__main__':
     #     print edgelist[0, j, 0, 1]
 
     # print 'x = ', x
-
-    # Input cell array of edgelists and tolerance.
-    seglist = np.asarray(lineseg2(edgelist, tol = 2))
-    print seglist.shape
